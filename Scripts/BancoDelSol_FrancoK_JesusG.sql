5ed0739bdd72e77021e8a60e2084087d45b9e044
Roberto García <rovertyto@outlook.com>
2024-02-20 08:16:18 -0700

    Store Procedure y trigger
    Funcionando desde la base de datos

--- Scripts/BancoDelSol_FrancoK_JesusG.sql
+++ Scripts/BancoDelSol_FrancoK_JesusG.sql
@@ -73,3 +73,63 @@ CREATE TABLE `transferencias` (
     FOREIGN KEY (`id_transaccion`) REFERENCES `transacciones`(`id_transaccion`),
     FOREIGN KEY (`cuenta_destino`) REFERENCES `cuentas`(`numero_cuenta`)
 );
+
+DELIMITER $$
+
+CREATE TRIGGER actualizar_saldo_despues_de_retiro_sin_cuenta
+AFTER INSERT ON retiros
+FOR EACH ROW
+BEGIN
+    DECLARE monto_retirado DOUBLE;
+	DECLARE cuenta_id BIGINT;
+    
+    -- Obtener el monto retirado a través de la tabla de transacciones
+    SELECT monto INTO monto_retirado
+    FROM transacciones
+    WHERE id_transaccion = NEW.id_transaccion;
+    
+    SELECT id_cuenta INTO cuenta_id
+    FROM transacciones
+    WHERE id_transaccion = NEW.id_transaccion;
+    -- Actualizar el saldo de la cuenta
+    UPDATE cuentas
+    SET saldo = saldo - monto_retirado
+    WHERE id_cuenta = cuenta_id;
+END;
+$$
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE generar_retiro_sin_cuenta(
+    IN folio_retiro VARCHAR(16),
+    IN contrasena_retiro VARCHAR(8),
+    IN cuenta_id BIGINT -- Nuevo parámetro para el ID de cuenta
+)
+BEGIN
+    DECLARE id_transaccion_retiro BIGINT;
+
+    -- Iniciar la transacción
+    START TRANSACTION;
+
+    -- Insertar el registro en la tabla de transacciones con el id_cuenta especificado
+    INSERT INTO transacciones (tipo, monto, id_cuenta) -- Asegúrate de incluir id_cuenta
+    VALUES ('Retiro', 0, cuenta_id);
+
+    -- Obtener el ID de la última transacción insertada
+    SET id_transaccion_retiro = LAST_INSERT_ID();
+
+    -- Insertar el registro en la tabla de retiros
+    INSERT INTO retiros (contrasena, folio, id_transaccion)
+    VALUES (contrasena_retiro, folio_retiro, id_transaccion_retiro);
+
+    -- Confirmar la transacción
+    COMMIT;
+END//
+
+DELIMITER ;
+
+
+SET autocommit = 0;
+
